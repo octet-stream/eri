d363a9ad2d1c5cdbabad963f478e60cfdb73f20c
Nick K <nick.kruchinin@gmail.com>
2020-08-14 00:49:42 +0300

    Small fix

--- model/PostTag/PostTag.js
+++ model/PostTag/PostTag.js
@@ -4,7 +4,7 @@ import createModel from "lib/db/createModel"
 
 import schema from "./schema"
 
-@createModel(schema, {paranoid: true})
+@createModel(schema)
 class PostTag extends Model {}
 
 export default PostTag
