cc901c62e7cf73af17daf343ba9265d01b39aae9
Nick K <nick.kruchinin@gmail.com>
2020-08-14 19:43:29 +0300

    Small fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,3 @@
-import db from "lib/db/connection"
-
-export async function getServerSideProps() {
-  await db.authenticate()
-
-  return {
-    props: {}
-  }
-}
-
 function Home() {
   return <div>Hello, world</div>
 }
