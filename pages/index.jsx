854fb812a3139e8d48a08f558f8ea18bd6b15e75
Nick K <nick.kruchinin@gmail.com>
2020-08-15 03:28:00 +0300

    Remove unnecessary code from main page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,3 @@
-import User from "model/User"
-
-export async function getServerSideProps() {
-  console.log(await User.findByPk(1))
-  return {
-    props: {}
-  }
-}
-
 function Home() {
   return <div>Hello, world</div>
 }
