1ada440d8b00f14f8c4876703b2aa3510a065c15
Nick K <nick.kruchinin@gmail.com>
2020-08-18 03:05:15 +0300

    Add post-to-tag n:m relation

--- server/api/resolve/query/post/post.js
+++ server/api/resolve/query/post/post.js
@@ -1,13 +1,22 @@
+import {Op as op} from "sequelize"
+
 import normalize from "lib/helper/graphql/normalizeParams"
 import db from "server/lib/db/connection"
 
 import Post from "server/model/Post"
 import User from "server/model/User"
-
-const include = [User]
+import Tag from "server/model/Tag"
+import PostsTags from "server/model/PostsTags"
 
 const getPost = ({args}) => db.transaction(async transaction => {
-  const post = await Post.findBySlug(args.slug, {transaction, include})
+  const {id, slug} = args
+
+  const post = await Post.findOne({
+    where: {[op.or]: [{id}, {slug}]},
+
+    include: [User, {model: Tag, through: PostsTags}],
+    transaction,
+  })
 
   if (!post) {
     throw new Error("Can't find a post")
--- server/model/Post/index.js
+++ server/model/Post/index.js
@@ -1,7 +1,12 @@
 import Post from "server/model/Post/Post"
 import User from "server/model/User/User"
+import Tag from "server/model/Tag/Tag"
+import PostsTags from "server/model/PostsTags"
 
 User.hasMany(Post)
 Post.belongsTo(User)
+
+Post.belongsToMany(Tag, {through: PostsTags})
+Tag.belongsToMany(Post, {through: PostsTags})
 
 export default Post
