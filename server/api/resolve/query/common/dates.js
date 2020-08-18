c5149a3a2ae837ea4c6f2a411777f635c39b6fcd
Nick K <nick.kruchinin@gmail.com>
2020-08-18 04:05:49 +0300

    Add docs for user and dates queries

--- server/api/resolve/query/common/dates.js
+++ server/api/resolve/query/common/dates.js
@@ -1,3 +1,16 @@
+/**
+ * @typedef {Object} Dates
+ *
+ * @property {Date} createdAt
+ * @property {Date} updatedAt
+ * @property {Date | null | undefined}  deletedAt
+ */
+
+/**
+ * @param {Dates} param
+ *
+ * @return {Dates}
+ */
 const dates = ({createdAt, updatedAt, deletedAt}) => ({
   createdAt, updatedAt, deletedAt
 })
--- server/api/resolve/query/user/name.js
+++ server/api/resolve/query/user/name.js
@@ -1,3 +1,15 @@
+/**
+ * @typedef {Object} UserName
+ *
+ * @property {string} firstName
+ * @property {string} lastName
+ */
+
+/**
+ * @param {UserName} param
+ *
+ * @return {UserName}
+ */
 const name = ({firstName, lastName}) => ({firstName, lastName})
 
 export default name
