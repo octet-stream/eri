a5969d3443da9207e1a99c3b5a64cb6cc36bfa5e
Nick K <nick.kruchinin@gmail.com>
2020-08-28 16:26:42 +0300

    Small fixes

--- server/api/resolve/query/post/creator.js
+++ server/api/resolve/query/post/creator.js
@@ -1,5 +1,15 @@
 import Post from "server/model/Post"
 
+/**
+ * @typedef {import("server/model/User").default} User
+ */
+
+/**
+ * @param {Post} post
+ * @param {{postId?: number}} args
+ *
+ * @return {Promise<User>}
+ */
 async function getCreator(post, {postId}) {
   if (!post) {
     post = await Post.findByPk(postId)
