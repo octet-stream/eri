bf3c2dc4ff42c8e955bea347deea853b69674df7
Nick K <nick.kruchinin@gmail.com>
2020-08-31 00:34:11 +0300

    Add client authentication.

--- component/Login/Login.jsx
+++ component/Login/Login.jsx
@@ -1,6 +1,9 @@
 import {useForm} from "react-hook-form"
 import {Fragment} from "react"
 
+import getCsrfToken from "lib/auth/getCsrfToken"
+import callAPI from "lib/rest/api"
+
 import Title from "component/Title"
 import Input from "component/Input"
 import Button from "component/Button"
@@ -10,7 +13,14 @@ import {container, box, fields, title} from "./login.module.css"
 function Login() {
   const {register, handleSubmit} = useForm()
 
-  const submit = credentials => console.log(credentials)
+  const submit = async credentials => callAPI
+    .post("auth/login", {
+      json: {
+        ...credentials, _csrf: await getCsrfToken()
+      }
+    })
+    .json()
+    .catch(console.error)
 
   return (
     <Fragment>
--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,9 +1,17 @@
+import {Op as op} from "sequelize"
+
+import createError from "http-errors"
 import nc from "next-connect"
+
+import add from "server/lib/auth/addSessionData"
 
 import onError from "server/middleware/errorHandler"
 import session from "server/middleware/session"
+import client from "server/middleware/client"
 import csrf from "server/middleware/csrf"
 import cors from "server/middleware/cors"
+
+import User from "server/model/User"
 
 export const config = {
   api: {
@@ -15,16 +23,52 @@ export const config = {
  * @param {import("http").IncomingMessage} req
  * @param {import("http").OutgoingMessage} res
  *
- * @return {void}
+ * @return {Promise<void>}
  */
 async function authenticate(req, res) {
-  res.send(JSON.stringify({message: "OK"}))
+  const {username, password} = req.body
+
+  const user = await User.findOne({
+    where: {
+      [op.or]: [
+        {
+          email: username
+        },
+        {
+          login: username
+        }
+      ]
+    }
+  })
+
+  if (!user) {
+    throw createError(401)
+  }
+
+  if (!(await user.comparePassword(password))) {
+    throw createError(401)
+  }
+
+  const {ip, browser, os} = req.client
+
+  // Set session data
+  add(req, {
+    userId: user.id,
+    clientIp: ip,
+    clientBrowserName: browser.name,
+    clientBrowserVersion: browser.version,
+    clientOsName: os.name,
+    clientOsVersion: os.version
+  })
+
+  res.send(JSON.stringify({message: "OK", error: null}))
 }
 
 const handler = nc({onError})
   .use(cors)
   .use(session)
   .use(csrf)
+  .use(client)
   .use(authenticate)
 
 export default handler
