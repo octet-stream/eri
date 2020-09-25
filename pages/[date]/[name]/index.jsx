05eccb4a12c9a221a8dd9a934318babac6a6d4ba
Nick K <nick.kruchinin@gmail.com>
2020-09-25 04:12:19 +0300

    Add minimal remark setup to render posts content directly to react components using remark-react

--- pages/[date]/[name]/index.jsx
+++ pages/[date]/[name]/index.jsx
@@ -1,6 +1,8 @@
 import {Fragment} from "react"
 
 import t from "prop-types"
+import remark from "remark"
+import toReact from "remark-react"
 
 import layout from "lib/hoc/layout"
 import exec from "lib/graphql/exec"
@@ -8,6 +10,8 @@ import getPost from "api/query/post.gql"
 
 import Title from "component/Title"
 import BlogLayout from "layout/Blog"
+
+const parser = remark().use(toReact)
 
 /**
  * @param {import("next").GetServerSidePropsContext} ctx
@@ -41,7 +45,7 @@ const Post = ({post}) => (
     <main>
       <h1>{post.title}</h1>
 
-      <article>{post.text}</article>
+      <article>{parser.processSync(post.text).result}</article>
     </main>
   </Fragment>
 )
