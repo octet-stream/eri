d5dd390516d348d71586dea9b1ce9a48ccff8016
Nick K <nick.kruchinin@gmail.com>
2021-01-31 18:04:55 +0300

    Small fix

--- component/Anchor/AnhorSlate.jsx
+++ component/Anchor/AnhorSlate.jsx
@@ -2,7 +2,7 @@ import {forwardRef} from "react"
 
 import Anchor from "./Anchor"
 
-/** @type {React.FC<React.HTMLAttributes<HTMLAnchorElement>>} */
+/** @type {React.FC<React.HTMLAttributes<HTMLAnchorElement> & {link: string}> */
 const AnchorSlate = forwardRef(({link, ...props}, ref) => (
   <Anchor {...props} ref={ref} href={link} />
 ))
