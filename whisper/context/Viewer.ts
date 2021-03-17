c14e53a0295a1d62dba3868093eb5c34eb2c4128
Nick K <nick.kruchinin@gmail.com>
2021-03-17 04:26:23 +0300

    whisper: Add Viewer context and useViewer hook

---
+++ whisper/context/Viewer.ts
@@ -0,0 +1,5 @@
+import {createContext} from "react"
+
+const Viewer = createContext<unknown>(null)
+
+export default Viewer
---
+++ whisper/hook/useViewer.ts
@@ -0,0 +1,23 @@
+import {UrlObject} from "url"
+
+import {useContext, useEffect} from "react"
+import {useRouter} from "next/router"
+
+import Viewer from "context/Viewer"
+
+type Url = UrlObject | string
+
+function useViewer(url: Url = "/") {
+  const viewer = useContext(Viewer)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!viewer) {
+      router.push(url)
+    }
+  }, [])
+
+  return viewer
+}
+
+export default useViewer
