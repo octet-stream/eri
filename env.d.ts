181449e716fbc01c3d9b3b85a4e6001cd2ab281a
Nick K <nick.kruchinin@gmail.com>
2022-07-16 18:46:03 +0300

    Add typings for more env variables to catch up with the .env.sample config

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,10 @@
 namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SERVER_URL: string
+
+    NEXTAUTH_URL: string
+    NEXTAUTH_SECRET: string
+
     MIKRO_ORM_DB_NAME: string
     MIKRO_ORM_HOST?: string
     MIKRO_ORM_PORT?: string
