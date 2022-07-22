44e910e497a2840118b4fe10d5de307f03258c9e
Nick K <nick.kruchinin@gmail.com>
2022-07-22 18:00:05 +0300

    Minor improvements for entities

--- src/server/db/entity/BaseDates.ts
+++ src/server/db/entity/BaseDates.ts
@@ -6,9 +6,15 @@ export type OptionalDates = "createdAt" | "updatedAt"
 
 @Entity({abstract: true})
 export abstract class BaseDates extends Base {
+  /**
+   * Date and time the entity was created
+   */
   @Property()
   readonly createdAt: Date = new Date()
 
+  /**
+   * Most recent date and time the entity was updated
+   */
   @Property({onUpdate: () => new Date()})
   readonly updatedAt: Date = new Date()
 }
--- src/server/db/entity/User.ts
+++ src/server/db/entity/User.ts
@@ -19,18 +19,33 @@ export enum UserRoles {
 export class User extends BaseDates {
   [OptionalProps]?: OptionalDates | "role"
 
+  /**
+   * User unique login
+   */
   @Property({unique: true})
   login!: string
 
+  /**
+   * User private email address
+   */
   @Property({unique: true})
   email?: string
 
+  /**
+   * The date and time user's email was verified
+   */
   @Property({nullable: true, hidden: true})
   emailVerified: Date | null = null
 
+  /**
+   * User password
+   */
   @Property({hidden: true})
   password!: string
 
+  /**
+   * User role
+   */
   @Enum({items: () => UserRoles, default: UserRoles.REGULAR})
   role: UserRoles = UserRoles.REGULAR
 
