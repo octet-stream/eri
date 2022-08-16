9b769e4c04b832b825823cf06c605d9b363e4a19
Nick K <nick.kruchinin@gmail.com>
2022-08-16 03:44:19 +0300

    Change test script, so docker compose down will run either if ava is failed or not

--- src/server/env.ts
+++ src/server/env.ts
@@ -2,4 +2,15 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import * as env from "@next/env"
 
-env.loadEnvConfig(process.cwd(), process.env.NODE_ENV !== "production")
+import {noop} from "lodash"
+
+env.loadEnvConfig(
+  process.cwd(),
+
+  process.env.NODE_ENV !== "production",
+
+  {
+    error: console.error.bind(console),
+    info: process.env.NODE_ENV === "test" ? noop : console.log.bind(console)
+  }
+)
