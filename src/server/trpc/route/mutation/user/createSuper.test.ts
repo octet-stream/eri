4af7012a432c3689ba1e57b288a9449c002283a7
Nick K <nick.kruchinin@gmail.com>
2022-08-24 18:49:39 +0300

    Add more tests for user.createSuper mutation

--- src/server/trpc/route/mutation/user/createSuper.test.ts
+++ src/server/trpc/route/mutation/user/createSuper.test.ts
@@ -1,3 +1,5 @@
+import {TRPCError} from "@trpc/server"
+
 import test from "ava"
 
 import {setup, cleanup} from "server/__helper__/database"
@@ -9,15 +11,38 @@ test.before(setup)
 
 test.after.always(cleanup)
 
-test("Creates a user with admin role", withTRPC, async (t, trpc, orm) => {
+test.serial("Creates a user with admin role", withTRPC, async (t, trpc, orm) => {
   const created = await trpc.mutation("user.createSuper", {
     login: "admin",
     email: "admin@example.com",
     password: "adminadminadmin"
   })
 
-  const user = await orm.em.findOne(User, created.id)
+  const user = await orm.em.findOneOrFail(User, created.id)
 
   t.not(user, null)
-  t.is(user?.role, UserRoles.SUPER)
+  t.is(user.role, UserRoles.SUPER)
+
+  await orm.em.removeAndFlush(user)
+})
+
+test("Fails if super user already exists", withTRPC, async (t, trpc, orm) => {
+  const user = orm.em.create(User, {
+    login: "admin",
+    email: "admin@example.com",
+    password: "adminadminadmin",
+    role: UserRoles.SUPER
+  })
+
+  await orm.em.persistAndFlush(user)
+
+  const trap = () => trpc.mutation("user.createSuper", {
+    login: "admin",
+    email: "admin@example.com",
+    password: "adminadminadmin"
+  })
+
+  const error = await t.throwsAsync(trap) as TRPCError
+
+  t.is(error.code, "NOT_FOUND")
 })
