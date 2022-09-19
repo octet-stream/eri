0efc274cd5b7f4bff4bf7dc576a7861f8e9cd847
Nick K <nick.kruchinin@gmail.com>
2022-09-19 17:46:31 +0300

    Small fix

--- src/lib/util/getEmptyPaths.ts
+++ src/lib/util/getEmptyPaths.ts
@@ -1,5 +1,8 @@
 import type {GetStaticPaths} from "next"
 
+/**
+ * Returns empty paths for `getStaticPaths` function allowing to skip pre-generation for pages with dynamic route.
+ */
 const getEmptyPaths: GetStaticPaths = async () => ({
   paths: [],
   fallback: "blocking"
