aae1cb422cbb95553c9782181a9721ad9185a72d
Nick K <nick.kruchinin@gmail.com>
2022-12-06 11:49:18 +0300

    Temporarily disable code block plugin

--- src/component/PostEditor/plugin/code.ts
+++ src/component/PostEditor/plugin/code.ts
@@ -1,9 +1,12 @@
-import {createCodePlugin, createCodeBlockPlugin} from "@udecode/plate-headless"
 import {createPlugins} from "@udecode/plate-core"
+import {
+  createCodePlugin,
+  // createCodeBlockPlugin
+} from "@udecode/plate-headless"
 
 import type {Value, Editor} from "lib/type/Editor"
 
 export const code = createPlugins<Value, Editor>([
   createCodePlugin(),
-  createCodeBlockPlugin()
+  // createCodeBlockPlugin()
 ])
