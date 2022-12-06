20110cea9e65ba82d5b882cfc15354dab87e4c13
Nick K <nick.kruchinin@gmail.com>
2022-12-06 11:48:03 +0300

    Add custom UI for link element

---
+++ src/component/PostEditor/element/Link.tsx
@@ -0,0 +1,15 @@
+import type {TRenderElementProps} from "@udecode/plate-core"
+import type {FC} from "react"
+
+import {Anchor} from "component/Anchor"
+
+import type {ILink} from "server/trpc/type/common/EditorData"
+import type {Value} from "lib/type/Editor"
+
+interface Props extends TRenderElementProps<Value, ILink> { }
+
+export const Link: FC<Props> = ({attributes, element, children}) => (
+  <Anchor {...attributes} href={element.url} className="text-[#0078d4] no-underline hover:underline">
+    {children}
+  </Anchor>
+)
--- src/component/PostEditor/plugin/link.ts
+++ src/component/PostEditor/plugin/link.ts
@@ -3,8 +3,11 @@ import {createPlugins} from "@udecode/plate-core"
 
 import type {Value, Editor} from "lib/type/Editor"
 
+import {Link} from "../element/Link"
+
 export const link = createPlugins<Value, Editor>([
   createLinkPlugin({
+    component: Link
     // renderAfterEditable: PlateFloatingLink
   })
 ])
