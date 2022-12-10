b7e1c8190ae571faaa77db1eee685ecffd4c70ba
Nick K <nick.kruchinin@gmail.com>
2022-12-10 18:42:31 +0300

    Transform value for Slug type

--- src/server/trpc/route/post/getBySlug.ts
+++ src/server/trpc/route/post/getBySlug.ts
@@ -8,8 +8,6 @@ import {getORM} from "server/lib/db"
 
 import procedure from "server/trpc/procedure/base"
 
-const {isArray} = Array
-
 /**
  * Returns a single post that matches given slug
  */
@@ -17,10 +15,8 @@ const getBySlug = procedure
   .input(z.object({slug: Slug}))
   .output(PostOutput)
   .query(async ({input}) => {
-    const slug = isArray(input.slug) ? input.slug.join("/") : input.slug
-
     const orm = await getORM()
-    const post = await orm.em.findOne(Post, {slug})
+    const post = await orm.em.findOne(Post, {slug: input.slug})
 
     if (!post) {
       throw new TRPCError({
--- src/server/trpc/type/common/Slug.ts
+++ src/server/trpc/type/common/Slug.ts
@@ -2,6 +2,8 @@ import {z} from "zod"
 
 import isDate from "validator/lib/isDate"
 
+const {isArray} = Array
+
 const SLUG_REGEXP = /^[a-z0-9-]+$/i
 
 const isDateValid = (date: string) => isDate(date, {format: "YYYY-MM-DD"})
@@ -17,4 +19,6 @@ export const SlugString = z.string().refine(slug => {
   return isDateValid(date) && SLUG_REGEXP.test(name)
 })
 
-export const Slug = z.union([SlugTuple, SlugString])
+export const Slug = z
+  .union([SlugTuple, SlugString])
+  .transform(slug => isArray(slug) ? slug.join("/") : slug)
