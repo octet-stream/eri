b16025c28e3330e740116971d8536f2da47ed760
Nick K <nick.kruchinin@gmail.com>
2022-12-13 15:48:57 +0300

    Add FloatingLink from plate ui

--- src/component/PostEditor/plugin/link.ts
+++ src/component/PostEditor/plugin/link.ts
@@ -1,13 +1,16 @@
+import {PlateFloatingLink} from "@udecode/plate-ui-link"
 import {createLinkPlugin} from "@udecode/plate-headless"
 import {createPlugins} from "@udecode/plate-core"
+
+import type {LinkPlugin} from "@udecode/plate-headless"
 
 import type {Value, Editor} from "lib/type/Editor"
 
 import {Link} from "../element/Link"
 
 export const link = createPlugins<Value, Editor>([
-  createLinkPlugin({
-    component: Link
-    // renderAfterEditable: PlateFloatingLink
+  createLinkPlugin<LinkPlugin, Value, Editor>({
+    component: Link,
+    renderAfterEditable: PlateFloatingLink as any // TODO: Replace with custom UI element
   })
 ])
