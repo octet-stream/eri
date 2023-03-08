308ab3a82c395d8ae84ef81101f87ff5061e995b
Nick K <nick.kruchinin@gmail.com>
2023-03-08 21:47:44 +0300

    Add custom slate-to-react transform for Link node

--- src/view/PostView/PostContent.tsx
+++ src/view/PostView/PostContent.tsx
@@ -5,12 +5,14 @@ import type {TPostOutput} from "server/trpc/type/output/PostOutput"
 
 import {usePageData} from "lib/hook/usePageData"
 
+import {Link} from "./element/Link"
+
 export const PostContent: FC = () => {
   const {content} = usePageData<TPostOutput>()
 
   return (
     <div className="flex-1">
-      <SlateView nodes={content} />
+      <SlateView nodes={content} transforms={{elements: [Link]}} />
     </div>
   )
 }
