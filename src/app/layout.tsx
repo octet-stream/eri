0b5d01b64d442e893a5d816a52b760148be8a6ee
Nick K <nick.kruchinin@gmail.com>
2023-05-07 01:00:04 +0300

    Group post layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,15 +18,13 @@ interface Props {
   children: ReactNode
 }
 
-const RootLayout: FC<Props> = ({children}) => (
+const BaseLayout: FC<Props> = ({children}) => (
   <html lang="en">
     <head />
 
     <body className="bg-white text-black dark:bg-gray-900 dark:text-white">
       <main className="w-screen min-h-screen flex">
-        <div className="prose dark:prose-invert py-5 laptop:p-5 mx-auto flex flex-1 flex-col dark:text-white">
-          {children}
-        </div>
+        {children}
       </main>
 
       <Toaster position="top-center" />
@@ -34,4 +32,4 @@ const RootLayout: FC<Props> = ({children}) => (
   </html>
 )
 
-export default RootLayout
+export default BaseLayout
