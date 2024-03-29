eaee1216a0e0760dd51492815bc7faf1a397d1b4
Nick K <nick.kruchinin@gmail.com>
2024-03-30 01:19:15 +0300

    test(remix): Add more tests for slug utils

--- app/server/lib/utils/slug.test.ts
+++ app/server/lib/utils/slug.test.ts
@@ -20,8 +20,32 @@ describe(formatSlugDate.name, () => {
 
 describe(formatSlugName.name, () => {
   test("Returns formatted name", () => {
-    const actual = formatSlugName("Hello world")
+    const actual = formatSlugName("Hello-world")
 
     expect(actual).toBe("hello-world")
+  })
+
+  test("Replaces colon symbol with the word", () => {
+    const actual = formatSlugName("Hello:world")
+
+    expect(actual).toBe("hello-colon-world")
+  })
+
+  test("Replaces @ (at) symbol with the word", () => {
+    const actual = formatSlugName("Hello@world")
+
+    expect(actual).toBe("hello-at-world")
+  })
+
+  test("Replaces comma symbol with the word", () => {
+    const actual = formatSlugName("Hello,world")
+
+    expect(actual).toBe("hello-comma-world")
+  })
+
+  test("Replaces period symbol with the word", () => {
+    const actual = formatSlugName("Hello.world")
+
+    expect(actual).toBe("hello-period-world")
   })
 })
