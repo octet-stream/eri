3f1ed5fce21ee3ac075bc6cdaa7ab06c529dc116
Nick K <nick.kruchinin@gmail.com>
2024-03-30 00:11:42 +0300

    feat(remix): Get env file from process.env.NODE_ENV

--- app/server/lib/db/cli.ts
+++ app/server/lib/db/cli.ts
@@ -1,3 +1,4 @@
-process.loadEnvFile(".env.development.local")
+// TODO: Improve env loading
+process.loadEnvFile(`.env.${process.env.NODE_ENV || "development"}.local`)
 
 export default import("./config.js").then(({config}) => config)
