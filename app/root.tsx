b78cedf8ff112d47cb32cbbb0512abcad8eb8c28
Nick K <nick.kruchinin@gmail.com>
2024-03-30 01:42:24 +0300

    chore(remix): Update root layout with new basic styles

--- app/root.tsx
+++ app/root.tsx
@@ -14,11 +14,15 @@ export const Layout = ({children}: { children: React.ReactNode }) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <Meta />
+
       <Links />
     </head>
-    <body>
+
+    <body className="bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100">
       {children}
+
       <ScrollRestoration />
+
       <Scripts />
     </body>
   </html>
