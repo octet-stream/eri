38e3459192d26e683a739b2e48b78a46bde478eb
Nick K <nick.kruchinin@gmail.com>
2024-04-01 00:20:26 +0300

    chore(admin/posts): Rename route: write -> posts.new

---
+++ app/routes/admin.posts.new.tsx
@@ -0,0 +1,5 @@
+import type {FC} from "react"
+
+const AdminPostNewPage: FC = () => <div>Post editor will be here</div>
+
+export default AdminPostNewPage
--- app/routes/admin.write.tsx
+++
@@ -1,5 +0,0 @@
-import type {FC} from "react"
-
-const AdminPostWritePage: FC = () => <div>Post editor will be here</div>
-
-export default AdminPostWritePage
