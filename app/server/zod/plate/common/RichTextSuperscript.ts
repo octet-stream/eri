44700e1ee0c5e752eece20e837ea71ebb7d46fa3
Nick K <nick.kruchinin@gmail.com>
2024-04-03 01:10:40 +0300

    fix(zod): Fix validation for plate text nodes and add tests for AnyText type

--- app/server/zod/plate/common/RichTextSuperscript.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod"
-
-export const RichTextSuperscript = z.object({
-  superscript: z.literal(true)
-})
-
-export type IRichTextSuperscript = z.input<typeof RichTextSuperscript>
-
-export type ORichTextSuperscript = z.output<typeof RichTextSuperscript>
