6d97304c80f3a21b9891c1def2c38bcf8a487769
Nick K <nick.kruchinin@gmail.com>
2024-04-03 15:27:44 +0300

    feat(admin): Re-export posts list loader from admin panel index

--- app/routes/admin._index.tsx
+++ app/routes/admin._index.tsx
@@ -1,3 +1,6 @@
+// TODO: Replace loader on this page with it's own when I add filters
+export {loader} from "./_index.jsx"
+
 const AdminPage = () => <div>Admin dashboard will be here</div>
 
 export default AdminPage
