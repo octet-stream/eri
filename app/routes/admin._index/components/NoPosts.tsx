e03761ea2c53489e7466492cd2abe3be3580963c
Nick K <nick.kruchinin@gmail.com>
2024-04-03 19:42:13 +0300

    feat(posts/admin): Add group for admin dashboard route and posts list

---
+++ app/routes/admin._index/components/NoPosts.tsx
@@ -0,0 +1,9 @@
+import type {FC} from "react"
+
+export const NoPosts: FC = () => (
+  <div className="flex flex-1 justify-center items-center">
+    <p className="text-sm text-muted-foreground">
+      There are no posts yet
+    </p>
+  </div>
+)
---
+++ app/routes/admin._index/components/PostItem.tsx
@@ -0,0 +1,14 @@
+import {Link} from "@remix-run/react"
+import type {FC} from "react"
+
+import {usePostContext} from "../contexts/PostContext.jsx"
+
+export const PostItem: FC = () => {
+  const {title, slug} = usePostContext()
+
+  return (
+    <Link to={`/admin/posts/${slug}`}>
+      {title}
+    </Link>
+  )
+}
