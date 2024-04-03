5495543f83435f10c2ae9199810dbde4d3b666bb
Nick K <nick.kruchinin@gmail.com>
2024-04-03 17:45:37 +0300

    feat(posts): Move index page into the folder to co-locate its components

---
+++ app/routes/_index/contexts/PostsContext.tsx
@@ -0,0 +1,20 @@
+import {createContext, useContext} from "react"
+
+import type {MaybeNull} from "../../../lib/types/MaybeNull.js"
+
+import {loader} from "../route.jsx"
+
+export type PostsContextData =
+  Awaited<ReturnType<Awaited<ReturnType<typeof loader>>["json"]>>
+
+export const PostsContext = createContext<MaybeNull<PostsContextData>>(null)
+
+export function usePostsContext(): PostsContextData {
+  const context = useContext(PostsContext)
+
+  if (!context) {
+    throw new Error("Unable to find PostsContext")
+  }
+
+  return context
+}
--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,8 +1,10 @@
 import {json, type MetaFunction} from "@remix-run/node"
 import {useLoaderData} from "@remix-run/react"
 
-import {Post} from "../server/db/entities.js"
-import {withOrm} from "../server/lib/db/orm.js"
+import {Post} from "../../server/db/entities.js"
+import {withOrm} from "../../server/lib/db/orm.js"
+
+import {NoPosts} from "./components/NoPosts.jsx"
 
 export const loader = withOrm(async orm => {
   const [list, count] = await orm.em.findAndCount(Post, {}, {
@@ -25,10 +27,12 @@ const HomePage = () => {
   const {count} = useLoaderData<typeof loader>()
 
   return (
-    <div className="w-full p-5 laptop:w-laptop laptop:mx-auto">
-      <div className="text-center">
-        {count > 0 ? "Posts will apper here" : "No posts yet"}
-      </div>
+    <div className="flex flex-col w-full p-5 laptop:w-laptop laptop:mx-auto">
+      {
+        count > 0
+          ? <div>Posts will apper here</div>
+          : <NoPosts />
+      }
     </div>
   )
 }
