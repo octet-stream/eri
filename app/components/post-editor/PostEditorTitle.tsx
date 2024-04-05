1fff2aaa95e8368b5a6daed84da150c4536abf02
Nick K <nick.kruchinin@gmail.com>
2024-04-05 19:50:31 +0300

    feat(editor): Use textarea for post title editor

--- app/components/post-editor/PostEditorTitle.tsx
+++ app/components/post-editor/PostEditorTitle.tsx
@@ -1,9 +1,31 @@
-import {Input} from "../ui/Input.jsx"
+import type {KeyboardEventHandler} from "react"
+import {useEvent} from "react-use-event-hook"
+
+import {Textarea} from "../ui/Textarea.jsx"
 
 import {usePostEditorContext} from "./PostEditorContext.jsx"
 
 export const PostEditorTitle = () => {
   const {register} = usePostEditorContext()
 
-  return <Input {...register("title")} placeholder="Title" />
+  // This prevents from inserting `\r\n` symbols to keep title a one-line
+  const preventReturn = useEvent<KeyboardEventHandler>(event => {
+    if (event.key.toLowerCase() === "enter") {
+      return event.preventDefault()
+    }
+  })
+
+  return (
+    <Textarea
+      {...register("title")}
+
+      className="resize-none"
+      placeholder="Title"
+      rows={1}
+      minRows={1}
+      maxRows={3}
+      maxLength={255}
+      onKeyDown={preventReturn}
+    />
+  )
 }
