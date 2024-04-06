eeb01a9dd3ba772b5c4e0f1e1090a6bd7c4b901b
Nick K <nick.kruchinin@gmail.com>
2024-04-06 03:10:17 +0300

    chore(editor): Adjustments for editor ui

--- app/components/post-editor/PostEditorContent.tsx
+++ app/components/post-editor/PostEditorContent.tsx
@@ -11,44 +11,52 @@ import {FloatingToolbar} from "../plate-ui/FloatingToolbar.jsx"
 import {FixedToolbar} from "../plate-ui/FixedToolbar.jsx"
 import {TooltipProvider} from "../plate-ui/Tooltip.jsx"
 import {Editor} from "../plate-ui/Editor.jsx"
+import {Label} from "../ui/Label.jsx"
 
 import {plugins} from "./plugins.js"
+import {PostEditorField} from "./PostEditorField.jsx"
 import {usePostEditorContext} from "./PostEditorContext.jsx"
 
 export const ContentEditor: FC = () => {
   const {control} = usePostEditorContext()
 
   return (
-    <Controller
-      name="content"
-      control={control}
-      render={({field: {name, value, onChange, onBlur}}) => {
-        const onDataChange = (
-          data: IPostContent
-        ) => onChange(JSON.stringify(data ?? []))
+    <PostEditorField className="flex-1">
+      <Label>
+        Content
+      </Label>
 
-        return (
-          <Fragment>
-            <DndProvider backend={HTML5Backend}>
-              <Plate plugins={plugins} value={value} onChange={onDataChange}>
-                <TooltipProvider>
-                  <FixedToolbar>
-                    <FixedToolbarButtons />
-                  </FixedToolbar>
+      <Controller
+        name="content"
+        control={control}
+        render={({field: {name, value, onChange, onBlur}}) => {
+          const onDataChange = (
+            data: IPostContent
+          ) => onChange(JSON.stringify(data ?? []))
 
-                  <Editor onBlur={onBlur} />
+          return (
+            <Fragment>
+              <DndProvider backend={HTML5Backend}>
+                <Plate plugins={plugins} value={value} onChange={onDataChange}>
+                  <TooltipProvider>
+                    <FixedToolbar>
+                      <FixedToolbarButtons />
+                    </FixedToolbar>
 
-                  <FloatingToolbar>
-                    <FloatingToolbarButtons />
-                  </FloatingToolbar>
-                </TooltipProvider>
-              </Plate>
-            </DndProvider>
+                    <Editor onBlur={onBlur} />
 
-            <input type="hidden" name={name} value={value || ""} />
-          </Fragment>
-        )
-      }}
-    />
+                    <FloatingToolbar>
+                      <FloatingToolbarButtons />
+                    </FloatingToolbar>
+                  </TooltipProvider>
+                </Plate>
+              </DndProvider>
+
+              <input type="hidden" name={name} value={value || ""} />
+            </Fragment>
+          )
+        }}
+      />
+    </PostEditorField>
   )
 }
--- app/components/post-editor/PostEditorTitle.tsx
+++ app/components/post-editor/PostEditorTitle.tsx
@@ -1,11 +1,15 @@
-import type {KeyboardEventHandler} from "react"
+import {useId, type KeyboardEventHandler} from "react"
 import {useEvent} from "react-use-event-hook"
 
 import {Textarea} from "../ui/Textarea.jsx"
+import {Label} from "../ui/Label.jsx"
 
+import {PostEditorField} from "./PostEditorField.jsx"
 import {usePostEditorContext} from "./PostEditorContext.jsx"
 
 export const PostEditorTitle = () => {
+  const id = useId()
+
   const {register} = usePostEditorContext()
 
   // This prevents from inserting `\r\n` symbols to keep title a one-line
@@ -16,16 +20,23 @@ export const PostEditorTitle = () => {
   })
 
   return (
-    <Textarea
-      {...register("title")}
+    <PostEditorField>
+      <Label htmlFor={id}>
+        Title
+      </Label>
 
-      className="resize-none"
-      placeholder="Title"
-      rows={1}
-      minRows={1}
-      maxRows={3}
-      maxLength={255}
-      onKeyDown={preventReturn}
-    />
+      <Textarea
+        {...register("title")}
+
+        id={id}
+        className="resize-none"
+        placeholder="What's your story?"
+        rows={1}
+        minRows={1}
+        maxRows={3}
+        maxLength={255}
+        onKeyDown={preventReturn}
+      />
+    </PostEditorField>
   )
 }
