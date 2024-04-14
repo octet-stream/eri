d62e536eafab96ca311bc0723a8e22f1a46a2b9b
Nick K <nick.kruchinin@gmail.com>
2024-04-14 22:22:38 +0300

    chore(server): Add a comment about caching

--- app/server/http/hono.ts
+++ app/server/http/hono.ts
@@ -6,6 +6,7 @@ import {remix} from "./middlewares/remix.js"
 
 const prod = process.env.NODE_ENV === "production"
 
+// TODO: Add cache middleware, like this: https://github.com/rphlmr/remix-hono-vite/blob/8c5938deca9adfe22de87cb87fc6983d3ffe6e4f/server/middlewares.ts
 export const hono = new Hono()
   .use("/assets/*", serveStatic({root: "./build/client"}))
   .use("*", serveStatic({root: prod ? "./build/client" : "./public"}))
