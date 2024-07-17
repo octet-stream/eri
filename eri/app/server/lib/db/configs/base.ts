cfd0b93d64e2e7cb879e4c959681f3b4f1c50191
Nick K <nick.kruchinin@gmail.com>
2024-07-17 04:42:41 +0300

    chore: Remove Mikro ORM seeder (for now)

--- eri/app/server/lib/db/configs/base.ts
+++ eri/app/server/lib/db/configs/base.ts
@@ -2,7 +2,6 @@ import {join, dirname} from "node:path"
 
 import {defineConfig} from "@mikro-orm/mysql"
 import {Migrator} from "@mikro-orm/migrations"
-import {SeedManager} from "@mikro-orm/seeder"
 
 import * as entities from "../../../db/entities.js"
 import * as subscribers from "../../../db/subscribers.js"
@@ -19,12 +18,9 @@ const config = defineConfig({
   ensureDatabase: true,
   entities: Object.values(entities),
   subscribers: Object.values(subscribers).map(Subscriber => new Subscriber()),
-  extensions: [Migrator, SeedManager],
+  extensions: [Migrator],
   migrations: {
     path: join(base, "migrations")
-  },
-  seeder: {
-    path: join(base, "seeder")
   }
 })
 
