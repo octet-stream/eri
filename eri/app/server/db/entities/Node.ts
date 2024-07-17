c474005b3d14c8f1e3ccb90a00a80106c0b6f5e9
Nick K <nick.kruchinin@gmail.com>
2024-07-17 04:56:36 +0300

    feat(db): Use uuid v7 for entities PK

--- eri/app/server/db/entities/Node.ts
+++ eri/app/server/db/entities/Node.ts
@@ -1,9 +1,10 @@
 import {PrimaryKey} from "@mikro-orm/mysql"
+import {v7 as uuidV7} from "uuid"
 
 /**
  * Represents base abstract database entity
  */
 export abstract class Node {
   @PrimaryKey({type: "uuid"})
-  readonly id: string = crypto.randomUUID()
+  readonly id: string = uuidV7()
 }
