8abfc114d19399e5087e6a9204790d3d591371a1
Nick K <nick.kruchinin@gmail.com>
2024-08-06 02:38:10 +0300

    chore: Rewrite logout action with conform

--- apps/eri/app/routes/admin.logout.ts
+++ apps/eri/app/routes/admin.logout.ts
@@ -1,6 +1,25 @@
-import {withTrpc} from "../server/trpc/withTrpc.js"
+import {redirect, type ActionFunction} from "@remix-run/node"
 
-export const loader = withTrpc(async trpc => trpc.admin.logout())
+import {parseCookie, removeCookie} from "../server/lib/auth/cookie.js"
+import {lucia} from "../server/lib/auth/lucia.js"
+
+export const loader: ActionFunction = async ({request}) => {
+  const sessionId = await parseCookie(request.headers.get("cookie"))
+
+  if (!sessionId) {
+    return redirect("/admin", {
+      status: 401
+    })
+  }
+
+  await lucia.invalidateSession(sessionId)
+
+  return redirect("/admin", {
+    headers: {
+      "set-cookie": await removeCookie()
+    }
+  })
+}
 
 export const action = (): never => {
   throw new Response(null, {
