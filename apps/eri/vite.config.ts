6153ca540d451a40df8ec9f6682025ddf43ce57e
Nick K <nick.kruchinin@gmail.com>
2024-08-05 21:11:28 +0300

    chore: Remove unused imports

--- apps/eri/vite.config.ts
+++ apps/eri/vite.config.ts
@@ -3,10 +3,7 @@ import {vitePlugin as remix} from "@remix-run/dev"
 import {installGlobals} from "@remix-run/node"
 import {defineConfig} from "vite"
 
-// import devServer from "@hono/vite-dev-server"
 import tsconfigPaths from "vite-tsconfig-paths"
-
-// import buildServer from "@eri-dev/vite-plugin-build-server"
 import buildMigrations from "@eri-dev/vite-plugin-mikro-orm-config"
 
 installGlobals()
