b58a0117d4620d6b8c0ad40866dd1f9aba8448d6
Nick K <nick.kruchinin@gmail.com>
2024-08-07 17:30:42 +0300

    feat(blog): Use single fetch for main page loader

--- apps/eri/app/routes/_blog._index/route.tsx
+++ apps/eri/app/routes/_blog._index/route.tsx
@@ -1,19 +1,64 @@
-import type {MetaFunction} from "@remix-run/node"
-import {useLoaderData} from "@remix-run/react"
+import {unstable_defineLoader as defineLoader} from "@remix-run/node"
+import {
+  type MetaArgs_SingleFetch as MetaArgs,
+  type MetaDescriptor,
+  useLoaderData
+} from "@remix-run/react"
 
 import {NoPosts} from "./components/NoPosts.jsx"
 import {PostsList} from "./components/PostsList.jsx"
 import {PostsContext} from "./contexts/PostsContext.jsx"
 
-import {withTrpc} from "../../server/trpc/withTrpc.js"
+import {Post} from "../../server/db/entities.js"
+import {PostListInput} from "../../server/zod/post/PostListInput.js"
+import {
+  PostListOutput,
+  type IPostListOutput
+} from "../../server/zod/post/PostListOutput.js"
 
-export const loader = withTrpc(async trpc => {
-  const page = await trpc.posts.getList()
+export const loader = defineLoader(async ({context: {orm}, request}) => {
+  const search = new URL(request.url).searchParams
+  const input = await PostListInput.safeParseAsync({
+    current: search.get("page")
+  })
 
-  return {page, title: process.env.BLOG_NAME || "Eri's Blog"}
+  if (!input.success) {
+    throw new Response(null, {
+      status: 404
+    })
+  }
+
+  const {args} = input.data
+  const [items, count] = await orm.em.findAndCount(
+    Post,
+
+    {},
+
+    {
+      offset: args.offset,
+      limit: args.limit
+    }
+  )
+
+  const output = await PostListOutput.parseAsync({
+    items,
+    count,
+    args
+  } satisfies IPostListOutput)
+
+  if (output.current < 1 || output.pagesCount < output.current) {
+    throw new Response(null, {
+      status: 404
+    })
+  }
+
+  return {
+    page: output,
+    title: process.env.BLOG_NAME || "Eri's blog"
+  }
 })
 
-export const meta: MetaFunction<typeof loader> = ({data}) => [
+export const meta = ({data}: MetaArgs<typeof loader>): MetaDescriptor[] => [
   {
     title: data?.title
   }
