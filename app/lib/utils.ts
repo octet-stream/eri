e7226932b8ecae4f5561f8d9d4e60997a733497b
Nick K <nick.kruchinin@gmail.com>
2024-08-08 18:08:00 +0300

    chore: Remove unused shadcn utils

--- app/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import {type ClassValue, clsx} from "clsx"
-import {twMerge} from "tailwind-merge"
-
-/**
- * @deprecated use `@udecode/cn` instead
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
