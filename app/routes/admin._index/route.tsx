a3a836d71db2783d94949d0aeb07cf8bc2eedf26
Nick K <nick.kruchinin@gmail.com>
2024-08-09 19:38:33 +0300

    chore: Use createPage utility on admin dashboard

--- app/routes/admin._index/route.tsx
+++ app/routes/admin._index/route.tsx
@@ -6,26 +6,17 @@ import {PostsContext} from "./contexts/PostsContext.jsx"
 
 import {defineAdminLoader} from "../../server/lib/admin/defineAdminLoader.server.js"
 
-import {PostListOutput} from "../../server/zod/post/PostListOutput.js"
-import {PostListInput} from "../../server/zod/post/PostListInput.js"
 import {Post} from "../../server/db/entities.js"
 
-import {parsePageInput} from "../../server/zod/utils/pagination/parsePageInput.js"
-import {parsePageOutput} from "../../server/zod/utils/pagination/parsePageOutput.js"
+import {PostPage} from "../../server/zod/post/PostPage.js"
 
 export const loader = defineAdminLoader(async ({request, context: {orm}}) => {
   const search = new URL(request.url).searchParams
-  const {args} = await parsePageInput(
-    PostListInput,
-    {
-      page: search.get("page")
-    },
+  const page = await PostPage.parseAsync({
+    page: search.get("page")
+  })
 
-    {
-      async: true
-    }
-  )
-
+  const {args} = page.params
   const [items, count] = await orm.em.findAndCount(
     Post,
 
@@ -37,21 +28,7 @@ export const loader = defineAdminLoader(async ({request, context: {orm}}) => {
     }
   )
 
-  const output = await parsePageOutput(
-    PostListOutput,
-
-    {
-      items,
-      count,
-      args
-    },
-
-    {
-      async: true
-    }
-  )
-
-  return output
+  return page.reply({items, count})
 })
 
 const AdminDashboardPage = () => {
