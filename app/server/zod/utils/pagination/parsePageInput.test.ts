a23dd2c6dd3ef3c245f5d1e9df3216acdd5c1224
Nick K <nick.kruchinin@gmail.com>
2024-08-13 10:40:54 +0300

    test(zod): Add tests for parsePageInput

---
+++ app/server/zod/utils/pagination/parsePageInput.test.ts
@@ -0,0 +1,25 @@
+import {test, expect} from "vitest"
+
+import {DefaultPageInput} from "./createPageInput.js"
+import {parsePageInput} from "./parsePageInput.js"
+import {PageArgs} from "./PageArgs.js"
+
+test("Returns default page input", () => {
+  const actual = parsePageInput(DefaultPageInput, {})
+
+  expect(actual).toBeTypeOf("object")
+  expect(actual.args).toBeInstanceOf(PageArgs)
+})
+
+test("Throws 404 Response error for invalid input", () => {
+  try {
+    parsePageInput(DefaultPageInput, {
+      page: "Invalid input"
+    })
+  } catch (error) {
+    const resp = error as Response
+
+    expect(resp).toBeInstanceOf(Response)
+    expect(resp.status).toBe(404)
+  }
+})
