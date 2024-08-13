94ae97c198fb3981b6811de1ada5cdecf9032ea9
Nick K <nick.kruchinin@gmail.com>
2024-08-13 10:06:25 +0300

    test(zod): Add tests for resolveResult

---
+++ app/server/zod/utils/resolveResult.test.ts
@@ -0,0 +1,41 @@
+import {test, expect, expectTypeOf, vi} from "vitest"
+import {z} from "zod"
+
+import {resolveResult} from "./resolveResult.js"
+
+test("Returns the result", () => {
+  const expected: string = "Hello, world!"
+  const actual = resolveResult(z.string().safeParse(expected))
+
+  expect(actual).toBe(expected)
+  expectTypeOf(actual).toBeString()
+})
+
+test("Throws default error Response", () => {
+  expect.assertions(2)
+
+  try {
+    resolveResult(z.string().safeParse([42, 451]))
+  } catch (error) {
+    const resp = error as Response
+
+    expect(resp).toBeInstanceOf(Response)
+    expect(resp.status).toBe(500)
+  }
+})
+
+test("Calls given onError function", () => {
+  expect.assertions(1)
+
+  const onError = vi.fn((): never => {
+    throw new Error("Something is broken")
+  })
+
+  try {
+    resolveResult(z.string().safeParse(false), {
+      onError
+    })
+  } catch {
+    expect(onError).toBeCalled()
+  }
+})
