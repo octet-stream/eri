0a592e0f3677bc9fde1b55dfe17c05ba9842efd4
Nick K <nick.kruchinin@gmail.com>
2024-08-22 15:44:23 +0300

    chore(app/config): Fix for DatabaseHost

--- app/server/lib/zod/config/orm/DatabaseHost.ts
+++ app/server/lib/zod/config/orm/DatabaseHost.ts
@@ -1,3 +1,3 @@
 import {z} from "zod"
 
-export const DatabaseHost = z.string().default("localhost") // TODO: Improve host validation
+export const DatabaseHost = z.string().min(1).default("localhost") // TODO: Improve host validation
