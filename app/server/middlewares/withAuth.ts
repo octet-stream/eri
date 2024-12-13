de4453be379dc5f1aabd3298c5076d04fb38297b
Nick K <nick.kruchinin@gmail.com>
2024-12-13 06:01:56 +0300

    chore: Remove unused comments

--- app/server/middlewares/withAuth.ts
+++ app/server/middlewares/withAuth.ts
@@ -2,15 +2,6 @@ import {createMiddleware} from "hono/factory"
 
 import {auth} from "../lib/auth/auth.js"
 
-// declare module "react-router" {
-//   interface AppLoadContext {
-//     /**
-//      * Auth context utilities
-//      */
-//     readonly auth: typeof auth
-//   }
-// }
-
 export const withAuth = () =>
   createMiddleware(async (ctx, next) => {
     ctx.set("auth", auth)
--- app/server/middlewares/withOrm.ts
+++ app/server/middlewares/withOrm.ts
@@ -1,17 +1,7 @@
 import {RequestContext} from "@mikro-orm/mariadb"
-import type {MikroORM} from "@mikro-orm/mariadb"
 import {createMiddleware} from "hono/factory"
 
 import {orm} from "../lib/db/orm.js"
-
-// declare module "react-router" {
-//   interface AppLoadContext {
-//     /**
-//      * Mikro ORM instance
-//      */
-//     readonly orm: MikroORM
-//   }
-// }
 
 export const withOrm = () =>
   createMiddleware(async (ctx, next) => {
