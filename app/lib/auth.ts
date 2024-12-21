34ad45d1d1c5fa8668c5d6a2e501fa06dfce2a05
Nick K <nick.kruchinin@gmail.com>
2024-12-21 09:56:00 +0300

    feat(auth): Add passkey plugin to better auth

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,3 +1,6 @@
+import {passkeyClient} from "better-auth/plugins/passkey"
 import {createAuthClient} from "better-auth/client"
 
-export const authClient = createAuthClient()
+export const authClient = createAuthClient({
+  plugins: [passkeyClient()]
+})
