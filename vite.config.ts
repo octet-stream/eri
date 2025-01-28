6c2c83d4ef8d1e3cab9a46eb4d99b5539a54a608
Nick K <nick.kruchinin@gmail.com>
2025-01-28 20:13:51 +0300

    chore(tooling): Add React Router dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import {reactRouter} from "@react-router/dev/vite"
+import {reactRouterDevTools} from "react-router-devtools"
 import {reactRouterHonoServer} from "react-router-hono-server/dev"
 import {defineConfig} from "vite"
 
 export default defineConfig({
-  plugins: [reactRouterHonoServer(), reactRouter()],
+  plugins: [reactRouterHonoServer(), reactRouterDevTools(), reactRouter()],
   optimizeDeps: {
-    exclude: [
-      "@node-rs/argon2",
-      "@mikro-orm/core",
-      "@mikro-orm/mariadb",
-      "@mikro-orm/knex",
-      "@mikro-orm/seeder",
-      "@mikro-orm/migrations"
-    ]
+    exclude: ["@node-rs/argon2"]
   },
   build: {
     target: "esnext"
