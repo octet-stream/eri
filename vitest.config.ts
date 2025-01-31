827a5299ac8792435cf7ce1b6f69a3461bd56e94
Nick K <nick.kruchinin@gmail.com>
2025-02-01 00:16:20 +0300

    chore: Remove test patterns from routes.ts and vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,5 @@
 import {defineConfig} from "vitest/config"
 
-/**
- * @deprecated The test are separated from the routes now, so this can be removed
- */
-export const TESTS_SEARCH_PATTERN = "tests/node/**/*.test.ts?(x)"
-
 export default defineConfig({
   test: {}
 })
