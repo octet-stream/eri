e5ee2cfdc9005a1e728e7ddc52705a9bd45106d6
Nick K <nick.kruchinin@gmail.com>
2025-02-04 13:56:43 +0300

    test(routes): Add tests for /admin/posts/new action and loader

---
+++ tests/node/routes/admin.posts.new.test.ts
@@ -0,0 +1,163 @@
+import {faker} from "@faker-js/faker"
+import {describe, expect} from "vitest"
+
+import {test} from "../../fixtures/admin.js"
+import {
+  createStubActionArgs,
+  createStubLoaderArgs
+} from "../../utils/createStubRouteArgs.js"
+
+import type {IPostContent} from "../../../app/server/zod/plate/editors/PostContent.js"
+import {createNodeId} from "../../../app/server/zod/plate/utils/nodeId.js"
+
+import {action, loader} from "../../../app/routes/admin.posts.new.jsx"
+import {Post} from "../../../app/server/db/entities.js"
+
+describe("loader", () => {
+  test("throws 401 for unauthorized access", async () => {
+    try {
+      await loader(createStubLoaderArgs())
+    } catch (error) {
+      const response = error as Response
+
+      expect(response.status).toBe(401)
+    }
+  })
+})
+
+describe("action", () => {
+  test("returns error when called with empty form", async ({admin}) => {
+    const form = new FormData()
+
+    const request = new Request(admin.request, {
+      method: "post",
+      body: form
+    })
+
+    const payload = await action(createStubActionArgs({request}))
+
+    expect(payload.init?.status).toBe(422)
+    expect(Object.keys(payload.data.error ?? {})).toEqual(["title", "content"])
+  })
+
+  test("redirects when post is created", async ({admin}) => {
+    const form = new FormData()
+
+    form.set("title", faker.lorem.sentence({min: 3, max: 5}))
+    form.set(
+      "content",
+
+      JSON.stringify([
+        {
+          id: createNodeId(),
+          type: "p",
+          children: [
+            {
+              id: createNodeId(),
+              text: faker.lorem.paragraph()
+            }
+          ]
+        }
+      ] satisfies IPostContent)
+    )
+
+    const request = new Request(admin.request, {
+      method: "post",
+      body: form
+    })
+
+    try {
+      await action(createStubActionArgs({request}))
+    } catch (error) {
+      const response = error as Response
+
+      expect(response.status).toBe(302)
+      expect(response.headers.get("x-remix-replace")).toBe("true")
+    }
+  })
+
+  test("location matches created post slug", async ({admin, orm}) => {
+    const form = new FormData()
+
+    form.set("title", faker.lorem.sentence({min: 3, max: 5}))
+    form.set(
+      "content",
+
+      JSON.stringify([
+        {
+          id: createNodeId(),
+          type: "p",
+          children: [
+            {
+              id: createNodeId(),
+              text: faker.lorem.paragraph()
+            }
+          ]
+        }
+      ] satisfies IPostContent)
+    )
+
+    const request = new Request(admin.request, {
+      method: "post",
+      body: form
+    })
+
+    try {
+      await action(createStubActionArgs({request}))
+    } catch (error) {
+      const response = error as Response
+
+      const location = response.headers.get("location")
+
+      expect(typeof location).toBe("string")
+      expect(location?.startsWith("/admin/posts/")).toBe(true)
+
+      const slug = location?.replace(/^\/admin\/posts\//, "")
+      const post = await orm.em.findOne(Post, {slug})
+
+      expect(post?.slug).toBe(slug)
+    }
+  })
+
+  test("created post has correct title and content", async ({admin, orm}) => {
+    const form = new FormData()
+
+    const title = faker.lorem.sentence({min: 3, max: 5})
+    const content: IPostContent = [
+      {
+        id: createNodeId(),
+        type: "p",
+        children: [
+          {
+            id: createNodeId(),
+            text: faker.lorem.paragraph()
+          }
+        ]
+      }
+    ]
+
+    form.set("title", title)
+    form.set("content", JSON.stringify(content))
+
+    const request = new Request(admin.request, {
+      method: "post",
+      body: form
+    })
+
+    try {
+      await action(createStubActionArgs({request}))
+    } catch (error) {
+      const response = error as Response
+
+      const location = response.headers.get("location")
+
+      expect(typeof location).toBe("string")
+      expect(location?.startsWith("/admin/posts/")).toBe(true)
+
+      const slug = location?.replace(/^\/admin\/posts\//, "")
+      const post = await orm.em.findOne(Post, {slug}, {populate: ["content"]})
+
+      expect(post).toMatchObject({title, content})
+    }
+  })
+})
