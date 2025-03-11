e4238f2bea1431369d38100135d07b006c32a913
Nick K <nick.kruchinin@gmail.com>
2025-03-11 18:57:20 +0300

    fix: Support new RR context API in test utils

--- tests/utils/createStubRouteArgs.ts
+++ tests/utils/createStubRouteArgs.ts
@@ -1,50 +1,48 @@
-import type {ActionFunctionArgs, LoaderFunctionArgs} from "react-router"
+import {unstable_RouterContextProvider as RouterContextProvider} from "react-router"
 
 import type {Replace} from "../../app/lib/types/Replace.js"
-import type {Variables} from "../../app/server.js"
 import {auth} from "../../app/server/lib/auth/auth.js"
 import {orm} from "../../app/server/lib/db/orm.js"
+import type {ActionArgs} from "../../app/server/lib/types/Action.js"
+import type {LoaderArgs} from "../../app/server/lib/types/Loader.js"
+
+import {serverContext} from "../../app/server/contexts/server.js"
 
 interface CreateStubRouteArgsInput<
-  TParams extends Record<string, unknown> = Record<string, unknown>,
-  TContext extends Record<string, unknown> = Record<string, unknown>
+  TParams extends Record<string, unknown> = Record<string, unknown>
 > {
   params?: TParams
   request?: Request
-  context?: TContext
+  context?: RouterContextProvider
 }
 
 const createStubRouteArgs =
-  <T extends LoaderFunctionArgs | ActionFunctionArgs>() =>
-  <
-    TParams extends Record<string, unknown> = Record<string, unknown>,
-    TContext extends Record<string, unknown> = Record<string, unknown>
-  >({
+  <T extends LoaderArgs | ActionArgs>() =>
+  <TParams extends Record<string, unknown> = Record<string, unknown>>({
     params,
     request,
-    context
-  }: CreateStubRouteArgsInput<TParams, TContext> = {}): Replace<
+    context = new RouterContextProvider()
+  }: CreateStubRouteArgsInput<TParams> = {}): Replace<
     T,
-    {params: TParams; context: TContext & Variables}
-  > =>
-    ({
-      request: request ?? new Request("http://localhost"),
-      context: {
-        resHeaders: new Headers(),
-        auth,
-        orm,
+    {
+      params: TParams
+    }
+  > => {
+    context.set(serverContext, {orm, auth, resHeaders: new Headers()})
 
-        ...context
-      },
+    return {
+      request: request ?? new Request("http://localhost"),
+      context,
       params: params ?? ({} as TParams)
-    }) as any // TypeScript complains about the type, but everything is ok in practice
+    } as any // TypeScript complains about the type, but everything is ok in practice. We can ignore this warning
+  }
 
 /**
  * Creates stub arguments for `loader`
  */
-export const createStubLoaderArgs = createStubRouteArgs<LoaderFunctionArgs>()
+export const createStubLoaderArgs = createStubRouteArgs<LoaderArgs>()
 
 /**
  * Creates stub arguments for `action`
  */
-export const createStubActionArgs = createStubRouteArgs<ActionFunctionArgs>()
+export const createStubActionArgs = createStubRouteArgs<ActionArgs>()
