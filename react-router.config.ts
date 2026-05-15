438ef42a4293192a43bfbdde1b7cf662b16fc69f
Nick K. <nick.kruchinin@gmail.com>
2026-05-15 23:39:29 +0300

    chore(deps): Bump dependencies

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,11 @@
 import type {Config} from "@react-router/dev/config"
 
 export default {
+  subResourceIntegrity: true,
   future: {
     v8_middleware: true,
     v8_splitRouteModules: true,
     v8_viteEnvironmentApi: true,
-    unstable_optimizeDeps: true,
-    unstable_subResourceIntegrity: true
+    unstable_optimizeDeps: true
   }
 } satisfies Config
